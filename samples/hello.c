446d7a3a9ba94d1d34a3e292b1c6c0632ffd697d
Kotaro Noguchi <kotaro.noguchi@linecorp.com>
2023-12-13 09:03:11 +0900

    Add struct and int[] to sample input

--- samples/hello.c
+++ samples/hello.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void callee() {
-    printf("Hello from callee\n");
+struct Person {
+    int age;
+    bool is_man;
+};
+
+void callee(char* msg, int num, struct Person person, int* arr) {
+    printf("%s", msg);
+    printf("passed num is %d\n", num);
+    printf("passed person is man? %d\n", person.is_man);
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
 int main() {
-    callee();
-    printf("hello\n");
+    int arr[3] = {1, 2, 3};
+    struct Person person = {20, true};
+    callee("Hi from callee\n", 99, person, arr);
 }
--- src/test.c
+++ src/test.c
@@ -8,12 +8,16 @@ int main() {
     size_t len = get_arg_count(map, "callee");
     printf("count of args in callee is %zu\n", len);
 
-    Arg second_arg = get_ith_arg(map, "callee", 1);
-    printf("call done\n");
-    printf("%s\n", second_arg.name);
-    printf("%llu\n", second_arg.bytes_cnt);
-    printf("%lld\n", second_arg.location);
-    printf("%s\n", second_arg.type_name);
+    for (int i = 0; i < len; ++i) {
+        printf("=== %d th arg ===\n", i+1);
+        Arg arg = get_ith_arg(map, "callee", i);
+
+        printf("name: %s\n", arg.name);
+        printf("bytes_cnt: %llu\n", arg.bytes_cnt);
+        printf("location: %lld\n", arg.location);
+        printf("type_name: %s\n", arg.type_name);
+        printf("\n");
+    }
 
     // Rustのfree_map関数を呼び出す
 //    free_map(map);
